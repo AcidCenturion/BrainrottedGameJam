09393e926ecb2b312cea2e22fe2149c8c0bf40c6
Michael Leong <acidcenturion@gmail.com>
2025-09-21 00:10:15 -0700

    dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,21 @@ using UnityEngine.InputSystem;
 
 public class PlatformerPlayerMove : MonoBehaviour
 {
-    //FIELDS
+    // FIELDS
     public float playerSpeed;
+    public float dashSpeed;
+    public float dashDuration;
+    public float dashMaxCooldown;
     public float jumpPower;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private float movementInput;
+    private bool dashInput;
+    private float dashTimer;
+    private float dashCooldown;
+    private float faceDirection;
+    private float originalGravity;
     private bool jumpInput;
     private RaycastHit2D groundCheck;
 
@@ -18,14 +26,52 @@ public class PlatformerPlayerMove : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalGravity = rb.gravityScale;
+        faceDirection = -1; // start facing left
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.linearVelocityX = movementInput * playerSpeed;
+        movement();
 
-Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.red);
+        if(dashInput && dashCooldown <= 0)
+        {
+            dashTimer = dashDuration;
+            dashCooldown = dashMaxCooldown;
+        }
+        dashCooldown -= Time.deltaTime;
+
+        jump();
+    }
+
+
+    // FUNCTIONS
+    private void movement()
+    {
+        //save the direction the player is facing
+        if(movementInput != 0)
+        {
+            faceDirection = movementInput;
+        }
+
+        if(dashTimer > 0)
+        {
+            rb.linearVelocityY = 0;
+            rb.gravityScale = 0;
+            rb.linearVelocityX = faceDirection * dashSpeed;
+            dashTimer -= Time.deltaTime;
+        }
+        else
+        {
+            rb.gravityScale = originalGravity;
+            rb.linearVelocityX = movementInput * playerSpeed;
+        }
+    }
+
+    private void jump()
+    {
+ Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.red);
         if (jumpInput && isGrounded())
         {
             rb.linearVelocityY = jumpPower;
@@ -33,7 +79,7 @@ Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.red);
     }
 
 
-    // Called by the Player Input component
+    // ACTION INPUT SYSTEM FUNCTIONS
     /*
     Gets the direction of the input 
     based on bindings connected to the Move action in the Input System asset
@@ -41,6 +87,12 @@ Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.red);
     private void OnMove(InputValue input)
     {
         movementInput = input.Get<float>();
+    }
+
+    private void OnDash(InputValue input)
+    {
+        dashInput = input.isPressed;
+Debug.Log("dash input");
     }
 
     /*
@@ -54,6 +106,8 @@ Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.red);
 Debug.Log("checked jump: " + jumpInput);
     }
 
+
+    // HELPER FUNCTIONS
     /*
     true when raycast detects the groundLayer
     */
