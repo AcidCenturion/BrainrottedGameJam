8157a23e92ed315674153176e7251156a540292c
antiantimoose <antiantigoose@gmail.com>
2025-09-22 00:57:47 -0700

    death screen transition

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public UIDocument DeathUIDoc;
+    public VisualElement Container;
+    public Button Respawn;
+
     public float maxHealth = 10f;
     public float currentHealth = 10f;
     public float damageScaler = 1.0f;
@@ -10,16 +16,23 @@ public class PlayerHealth : MonoBehaviour
 
     public int CollectedLightbulbsNumber = 0;
 
+    private bool hasDied = false;
+    private float targetOpacity = 0f; 
+    private float lerpSpeed = 2f;
+
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        Container = DeathUIDoc.rootVisualElement.Q<VisualElement>("Container");
+        Respawn = DeathUIDoc.rootVisualElement.Q<Button>("Respawn");
     }
 
     void FixedUpdate()
     {
 
         //Debug.Log("health: " + currentHealth);
-
+        //Debug.Log(i);
         if (currentHealth >= 0)
         {
             if (isSafe == false)
@@ -27,12 +40,24 @@ public class PlayerHealth : MonoBehaviour
                 currentHealth -= Time.deltaTime * damageScaler;
             }
         }
-        else
+
+        if (currentHealth <= 0 && !hasDied)
         {
+            hasDied = true;
+            targetOpacity = targetOpacity == 1f ? 0f : 1f;
+            Debug.Log("DeathStarted");
             Die();
         }
- 
+
+        if (hasDied)
+        {
+            float currentOpacity = Container.resolvedStyle.opacity;
+            float newOpacity = Mathf.Lerp(currentOpacity, targetOpacity, lerpSpeed * Time.deltaTime);
+            Container.style.opacity = newOpacity;
+        }   
     }
+ 
+    
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -55,7 +80,10 @@ public class PlayerHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("player dead bro");
-        Destroy(gameObject);
+        // rb.linearVelocityX = 0;
+        // rb.linearVelocityY = 0;
+
+
     }
 
 
